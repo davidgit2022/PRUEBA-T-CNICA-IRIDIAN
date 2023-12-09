88c671f8c10a871d1b770473b80bdf2909904bba
Deivy Potosi <deibypotosi535@gmail.com>
2023-12-08 19:45:42 -0500

    Antes de correr las migraciones

---
+++ app/Models/Contact.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Contact extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'last_name',
+        'email',
+        'phone',
+        'area_id'
+    ];
+
+    public function area():BelongsTo
+    {
+        return $this->belongsTo(Area::class);
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Area;
+use App\Models\Contact;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        Area::factory(10)->create();
+        Contact::factory(50)->create();
+    }
+}
